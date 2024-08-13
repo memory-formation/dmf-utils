b05e9305f53d7baaec2975df3f99026d1dc6d38f
pablomm <pablomm@users.noreply.github.com>
2024-08-13 23:58:59 +0200

    Rename helper.py to decorator.py

--- dmf/alerts/__init__.py
+++ dmf/alerts/__init__.py
@@ -5,6 +5,6 @@ This package contains utilities for sending notifications.
 """
 
 from .alerts import send_alert, send_message, get_backend
-from .helper import alert
+from .decorator import alert
 
 __all__ = ["alert", "send_alert", "send_message", "get_backend"]
