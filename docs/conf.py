76e9f000c19f90ebed52e85eb6ce4d0acc290f7a
pablomm <pablomm@users.noreply.github.com>
2024-08-15 00:11:07 +0200

    Add structure of the documentation

---
+++ docs/conf.py
@@ -0,0 +1,86 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+import pkg_resources
+
+sys.path.append(os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+try:
+    release = pkg_resources.get_distribution("dmf-utils").version
+except pkg_resources.DistributionNotFound:
+    print(
+        "To build the documentation, the distribution information of "
+        "dmf-utils has to be available. Either install the package "
+        'into your development environment or run "setup.py develop" '
+        "to setup the metadata. A virtualenv is recommended. "
+    )
+    sys.exit(1)
+
+project = "dmf-utils"
+copyright = "2024, Dynamics of Memory Formation Group, UB"
+author = "Dynamics of Memory Formation Group (DMF)"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+]
+
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Add mappings
+intersphinx_mapping = {
+    "python": ("http://docs.python.org/3", None),
+}
+
+autodoc_default_options = {"members": True, "inherited-members": True, "show-inheritance": True}
+
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_favicon = "https://brainvitge.org/website/wp-content/themes/brainvitge/favicon.png"
+html_logo = "https://raw.githubusercontent.com/memory-formation/.github/main/logos/brainvitge.png"
+html_theme_options = {
+    "github_url": "https://github.com/memory-formation/dmf-utils",
+    # "navbar_start": ["navbar-logo"],
+    "logo": {
+        "text": "dmf-utils",
+    },
+    "icon_links": [
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/dmf-utils",
+            "icon": "fab fa-python",
+        },
+         {
+            "name": "DMF",
+            "url": "https://brainvitge.org/groups/memory_formation/",
+            "icon": "https://brainvitge.org/website/wp-content/themes/brainvitge/library/images/brainvitge-logo-45x40.png",
+            "type": "url",
+        },
+    ],
+}
+html_context = {
+    "github_user": "memory-formation",
+    "github_repo": "dmf-utils",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
---
+++ docs/index.rst
@@ -0,0 +1,77 @@
+Welcome to DMF Utils's documentation!
+=====================================
+
+.. image:: https://img.shields.io/badge/python-3.7%20|%203.8%20|%203.9%20|%203.10%20|%203.11%20|%203.12-blue.svg
+   :alt: Python Versions
+
+.. image:: https://img.shields.io/badge/license-MIT-green.svg
+   :target: https://github.com/memory-formation/dmf-utils/blob/main/LICENSE
+   :alt: License
+
+----------------
+
+DMF Utils is a Python package that provides a collection of utility functionalities to 
+simplify common tasks in experiment and data analysis workflows. 
+The package contains modules used by our group to facilitate tasks in neuroscience and artificial intelligence research.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Content
+
+   installation
+   modules/index
+
+Quick Start
+-----------
+.. _quick-start:
+
+DMF Utils is designed in a modular way, allowing you to install only the components needed for a specific project. 
+This modularity also ensures compatibility with a wide range of Python versions.
+
+To install all modules:
+
+.. code-block:: bash
+
+    pip install dmf-utils[all]
+
+See the :doc:`installation` section for more details.
+
+You can use DMF Utils to send alerts and manage experiments efficiently. For example, here is how you can use the Alerts module:
+
+.. code-block:: python
+
+    from dmf.alerts import alert
+
+    @alert
+    def my_function(name):
+        sleep(5)
+        return f"Hello, {name}!"
+
+    my_function("World")
+
+Or from the command line:
+
+.. code-block:: bash
+
+    ./my_function > output.txt
+    dmf-alert "Execution finished" -a output.txt
+
+For more information on setting up the messaging service and using other functionalities, see the :doc:`modules/index` section.
+
+Contributing
+------------
+
+DMF Utils is maintained by the DMF Group at the University of Barcelona. We welcome contributions from the community. If you would like to contribute, open an issue or submit a pull request on our `GitHub repository <https://github.com/memory-formation/dmf-utils>`
+
+License
+-------
+
+DMF Utils is licensed under the MIT License. See the license file for more details.
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
